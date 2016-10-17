76ca65b36c5ae40f93d657de049e454819c62d09
Meghana Arvind <meghana.arvind@gmail.com>
2016-10-17 14:18:35 -0400

    fb1

---
+++ app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Feed extends React.Component {
+		render() {
+			return (
+				<p>ugh,world</p>
+			);
+		}
+}
+
+ReactDOM.render(
+	<UghWorld />
+	document.getElementById('fb-feed')
+)
